05318f6113f8da87824e1a7ce0038f617484619d
BatKa <batka.studies@gmail.com>
2026-05-18 20:00:30 +0800

    Toglogchiin onoog huvaarildag bolgoson

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглэе
 
-var activePlayer = 1;
+var activePlayer = 0;
 
 // Тоглогчийн цуглуулсан оноог хадгалах хувьсагч
 
@@ -20,11 +20,45 @@ document.getElementById('current-1').textContent = '0';
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
 
+// Шоог шидэх эвент листенер
 document.querySelector(".btn-roll").addEventListener("click", function() {
+    // 1-6 хүртэлх санамсаргүй тоо гаргаж авах
     var diceNumber = Math.floor(Math.random() * 6) + 1;
 
+    // Шооны зургыг гаргаж ирэх
     diceDom.style.display = "block";
+
+    // Буусан санамсаргүй тооноос хамаарч шооны зургийг вэб дээр гаргаж ирэх
     diceDom.src = 'dice-' + diceNumber + '.png'; 
-    
-    // alert("Шоо буулаа : " + diceNumber);
+
+    // Шоо буусанаас хамаарч оноог өөрчилнө
+    if(diceNumber !== 1) {
+        // 1-ээс ялгаатай тоо буулаа. Буусан тоог тоглогчид нэмж өгнө.
+        roundScore = roundScore + diceNumber;
+        document.getElementById("current-" + activePlayer).textContent = roundScore;
+    } else {
+        // 1 буусан тул тоглогчийн ээлжийг энэ хэсэгт сольж өгнө.
+
+        // Энэ тоглогчийн ээлжиндээ цуглуулсан оноог 0 болгоно.
+
+        roundScore = 0;
+        document.getElementById("current-" + activePlayer).textContent = 0;
+        // Хэрэв идэвхтэй тоглогч 0 байвал идэвхтэй тоглогчийг 1 болго
+        // Үгүй бол идэвхтэй тоглогч 0 байвал идэвхтэй тоглогчийг 1 болго
+
+        activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+        // Улаан цэгийг шилжүүлэх
+        document.querySelector(".player-0-panel").classList.toggle("active");
+        document.querySelector(".player-1-panel").classList.toggle("active");
+
+        // Шоог түр алга болгох
+        diceDom.style.display = "none";
+
+        // if(activePlayer == 0) {
+        //     activePlayer = 1;
+        // } else [
+        //     activePlayer = 0;
+        // }
+    }
 });
