e7af85b81fffdd45221c2a71349c51f5bebafd09
BatKa <batka.studies@gmail.com>
2026-05-18 12:51:22 +0800

    Shoo shiddeg bolgoh

--- app.js
+++ app.js
@@ -12,14 +12,19 @@ var roundScore = 0;
 
 // Шоны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг нь хувьсагчид санамсаргүй үүсгэж өгнө.
 
-var dice = Math.floor(Math.random() * 6) + 1;
+document.getElementById("score-0").textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
+document.querySelector(".btn-roll").addEventListener("click", function() {
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector(".dice").style.display = "none";
-
-console.log('Шоо ' + dice);
+    diceDom.style.display = "block";
+    diceDom.src = 'dice-' + diceNumber + '.png'; 
+    
+    // alert("Шоо буулаа : " + diceNumber);
+});
